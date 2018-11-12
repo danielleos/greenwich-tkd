32a3c4112d066d51997b99eabb54dde93f197c35
danielle.osullivan <danielle.osullivan@accenture.com>
2018-11-12 22:28:47 +0000

    Add yul gok & joong gun translations

--- js/custom.js
+++ js/custom.js
@@ -19,8 +19,8 @@ Wen \
 	<li>Wen gunnun so ap joomuk kaunde baro ap jirugi</li> \
 	<li>Orun gunnun so bakat palmok najunde bakuro baro yop makgi</li> \
 	<li>Wen gunnun so ap joomuk kaunde baro ap jirugi</li> \
-	<li>Narani junbi sogi (wenbal e orunbal)</li> \
 </ol> \
+Narani junbi sogi (wenbal e orunbal) \
 ";
 var saju_jirugi_english = "Ready posture: Parallel ready stance<br> \
 Right \
@@ -43,8 +43,8 @@ Wen \
 	<li>Left walking stance forefist middle obverse front punch</li> \
 	<li>Right walking stance outer forearm low outward obverse side block</li> \
 	<li>Left walking stance forefist middle obverse front punch</li> \
-	<li>Parallel ready stance (left foot to right foot)</li> \
 </ol> \
+Parallel ready stance (left foot to right foot) \
 ";
 
 var saju_makgi_korean = "Junbi jase: Narani junbi sogi<br> \
@@ -70,8 +70,8 @@ Wen \
 	<li>Wen gunnun so an palmok kaunde bakuro baro yop makgi</li> \
 	<li>Orun gunnun so sonkal najunde bakuro baro yop makgi</li> \
 	<li>Wen gunnun so an palmok kaunde bakuro baro yop makgi</li> \
-	<li>Narani junbi sogi (wenbal e orunbal)</li> \
 </ol> \
+Narani junbi sogi (wenbal e orunbal) \
 ";
 var saju_makgi_english = "Ready posture: Parallel ready stance<br> \
 Right \
@@ -96,8 +96,8 @@ Wen \
 	<li>Left walking stance inner forearm middle outward obverse side block</li> \
 	<li>Right walking stance knife-hand low outward obverse side block</li> \
 	<li>Left walking stance inner forearm middle outward obverse side block</li> \
-	<li>Parallel ready stance (left foot to right foot)</li> \
 </ol> \
+Parallel ready stance (left foot to right foot) \
 ";
 
 var chonji_korean = "Junbi jase: Narani junbi sogi<br> \
@@ -121,8 +121,8 @@ var chonji_korean = "Junbi jase: Narani junbi sogi<br> \
 	<li>Orun gunnun so kaunde jirugi</li> \
 	<li>Wen gunnun so kaunde jirugi</li> \
 	<li>Orun gunnun so kaunde jirugi</li> \
-	<li>Narani junbi sogi (wenbal e orunbal)</li> \
 </ol> \
+Narani junbi sogi (wenbal e orunbal) \
 ";
 var chonji_english = "Ready posture: Parallel ready stance<br> \
 <ol> \
@@ -145,8 +145,8 @@ var chonji_english = "Ready posture: Parallel ready stance<br> \
 	<li>Right walking stance middle punch</li> \
 	<li>Left walking stance middle punch</li> \
 	<li>Right walking stance middle punch</li> \
-	<li>Parallel ready stance (left foot to right foot)</li> \
 </ol> \
+Parallel ready stance (left foot to right foot) \
 ";
 
 var dan_gun_korean = "Junbi jase: Narani junbi sogi<br> \
@@ -172,8 +172,8 @@ var dan_gun_korean = "Junbi jase: Narani junbi sogi<br> \
 	<li>Orun gunnun so nopunde jirugi</li> \
 	<li>Wen niunja so sonkal kaunde bakuro taerigi</li> \
 	<li>Wen gunnun so nopunde jirugi</li> \
-	<li>Narani junbi sogi (wenbal e orunbal)</li> \
 </ol> \
+Narani junbi sogi (wenbal e orunbal) \
 ";
 var dan_gun_english = "Ready posture: Parallel ready stance<br> \
 <ol> \
@@ -198,15 +198,15 @@ var dan_gun_english = "Ready posture: Parallel ready stance<br> \
 	<li>Right walking stance high punch</li> \
 	<li>Left L-stance knife-hand middle outward strike</li> \
 	<li>Left walking stance high punch</li> \
-	<li>Parallel ready stance (left foot to right foot)</li> \
 </ol> \
+Parallel ready stance (left foot to right foot) \
 ";
 
 var do_san_korean = "Junbi jase: Narani junbi sogi<br> \
 <ol> \
 	<li>Wen gunnun so bakat palmok bakuro nopunde yop makgi</li> \
 	<li>Wen gunnun so kaunde bandae jirugi</li> \
-	<li>(Gujari dolgi) orun gunnun so bakat palmok bakuro nopunde yop makgi</li> \
+	<li>(Dwiro dora, gujari dolgi) orun gunnun so bakat palmok bakuro nopunde yop makgi</li> \
 	<li>Orun gunnun so kaunde bandae jirugi</li> \
 	<li>Orun niunja so sonkal kaunde daebi makgi</li> \
 	<li>Orun gunnun so kaunde sun sonkut tulgi</li> \
@@ -214,7 +214,7 @@ var do_san_korean = "Junbi jase: Narani junbi sogi<br> \
 	<li>Orun gunnun so dung joomuk nopunde yop taerigi</li> \
 	<li>Wen gunnun so bakat palmok bakuro nopunde yop makgi</li> \
 	<li>Wen gunnun so kaunde bandae jirugi</li> \
-	<li>(Gujari dolgi) orun gunnun so bakat palmok bakuro nopunde yop makgi</li> \
+	<li>(Dwiro dora, gujari dolgi) orun gunnun so bakat palmok bakuro nopunde yop makgi</li> \
 	<li>Orun gunnun so kaunde bandae jirugi</li> \
 	<li>Wen gunnun so bakat palmok nopunde hechyo makgi</li> \
 	<li>Orun kaunde apcha busigi (son 13 lo)</li> \
@@ -228,8 +228,8 @@ var do_san_korean = "Junbi jase: Narani junbi sogi<br> \
 	<li>Orun gunnun so bakat palmok chookyo makgi</li> \
 	<li>Annun so wen sonkal kaunde yop taerigi</li> \
 	<li>Annun so orun sonkal kaunde yop taerigi</li> \
-	<li>Narani junbi sogi (orunbal e wenbal)</li> \
 </ol> \
+Narani junbi sogi (orunbal e wenbal) \
 ";
 var do_san_english = "Ready posture: Parallel ready stance<br> \
 <ol> \
@@ -239,8 +239,8 @@ var do_san_english = "Ready posture: Parallel ready stance<br> \
 	<li>Right walking stance middle reverse punch</li> \
 	<li>Right L-stance knife-hand middle guarding block</li> \
 	<li>Right walking stance middle straight fingertip thrust</li> \
-	<li>Release from grab, left walking stance backfist high side strike<li> \
-	<li>Right walking stance backfist high side strike</li> \
+	<li>Release from grab, left walking stance back-fist high side strike<li> \
+	<li>Right walking stance back-fist high side strike</li> \
 	<li>Left walking stance outer forearm outward high side block</li> \
 	<li>Left walking stance middle reverse punch</li> \
 	<li>(Spot turn) right walking stance outer forerarm outward high side block</li> \
@@ -257,8 +257,8 @@ var do_san_english = "Ready posture: Parallel ready stance<br> \
 	<li>Right walking stance outer forearm rising block</li> \
 	<li>Sitting stance left knife-hand middle side strike</li> \
 	<li>Sitting stance right knife-hand middle side strike</li> \
-	<li>Parallel ready stance (right foot to left foot)</li> \
 </ol> \
+Parallel ready stance (right foot to left foot) \
 ";
 
 var won_hyo_korean = "Junbi jase: Moa junbi sogi A<br> \
@@ -270,7 +270,7 @@ var won_hyo_korean = "Junbi jase: Moa junbi sogi A<br> \
 	<li>Wen niunja nopunde sonkal anuro taerigi (gwa orun joomuk e wen okkae)</li> \
 	<li>Orun gojung so kaunde yop jirugi</li> \
 	<li>(Orunbal e wenbal) orun guburyo junbi sogi A</li> \
-	<li>Wen kaunde yop cha jirugi</li> \
+	<li>Wen kaunde yopcha jirugi</li> \
 	<li>Orun niunja so sonkal kaunde daebi makgi</li> \
 	<li>Wen niunja so sonkal kaunde daebi makgi</li> \
 	<li>Orun niunja so sonkal kaunde daebi makgi</li> \
@@ -282,17 +282,17 @@ var won_hyo_korean = "Junbi jase: Moa junbi sogi A<br> \
 	<li>Wen niunja so nopunde sonkal anuro taerigi</li> \
 	<li>Orun gojung so kaunde yop jirugi</li> \
 	<li>Wen gunnun so an palmok dollimyo makgi</li> \
-	<li>Orun najunde ap cha busigi (son 19 lo)</li> \
+	<li>Orun najunde apcha busigi (son 19 lo)</li> \
 	<li>Orun gunnun so kaunde bandae jirugi</li> \
 	<li>Orun gunnun so an palmok dollimyo makgi</li> \
-	<li>Wen najunde ap cha busigi (son 22 lo)</li> \
+	<li>Wen najunde apcha busigi (son 22 lo)</li> \
 	<li>Wen gunnun so kaunde bandae jirugi</li> \
 	<li>Wen goburyo junbi sogi A</li> \
-	<li>Orun kaunde yop cha busigi</li> \
+	<li>Orun kaunde yopcha busigi</li> \
 	<li>Orun niunja so kaunde palmok daebi makgi</li> \
 	<li>Wen niunja so kaunde palmok daebi makgi</li> \
-	<li>Moa junbi sogi A (orunbal e wenbal)</li> \
 </ol> \
+Moa junbi sogi A (orunbal e wenbal) \
 ";
 var won_hyo_english = "Ready posture: Closed ready stance A<br> \
 <ol> \
@@ -324,8 +324,170 @@ var won_hyo_english = "Ready posture: Closed ready stance A<br> \
 	<li>Right middle side piercing kick</li> \
 	<li>Right L-stance middle forearm guarding block</li> \
 	<li>Left L-stance middle forearm guarding block</li> \
-	<li>Closed ready stance A (right foot to left foot)</li> \
 </ol> \
+Closed ready stance A (right foot to left foot) \
+";
+
+var yul_gok_korean = "Junbi jase: Narani junbi sogi<br> \
+<ol> \
+	<li>Annun sogi (chukjong wen joomuk, podon guro)</li> \
+	<li>Annun so kaunde orun ap joomuk jirugi (barun donjak)</li> \
+	<li>Annun so kaunde wen ap joomuk jirugi (barun dongjak)</li> \
+	<li>(Wenbal e orunbal) annun sogi (chukjong orun joomuk, podon guro)</li> \
+	<li>Annun so kaunde wen ap joomuk jirugi (barun dongjak)</li> \
+	<li>Annun so kaunde orun ap joomuk jirugi (barun dongjak)</li> \
+	<li>Orun gunnun so kaunde an palmok yop makgi</li> \
+	<li>Wen najunde apcha busigi (son 7 lo)</li> \
+	<li>Wen gunnun so kaunde baro ap joomuk jirgui (barun dongjak)</li> \
+	<li>Wen gunnun so kaunde bandae ao joomuk jirugi (barun dongjak)</li> \
+	<li>Wen gunnun so kaunde an palmok yop makgi</li> \
+	<li>Orun najunde apcha busigi (son 11 lo)</li> \
+	<li>Orun gunnun so kaunde baro ap joomuk jirugi (barun dongjak)</li> \
+	<li>Orun gunnun so kaunde bandae ap joomuk jirugi (barun dongjak)</li> \
+	<li>Orun gunnun so kaunde baro sonbadak golcho makgi</li> \
+	<li>Orun gunnun so kaunde bandae sonbadak golcho makgi (yongyol dongjak)</li> \
+	<li>Orun gunnun so kaunde baro ap joomuk jirugi (yongyol dongjak)</li> \
+	<li>Wen gunnun so kaunde baro sonbadak golcho makgi</li> \
+	<li>Wen gunnun so kaunde bandae sonbadak golcho makgi (yongyol dongjak)</li> \
+	<li>Wen gunnun so kaunde baro ap joomuk jirugi (yongyol dongjak)</li> \
+	<li>Orun gunnun so kaunde baro ap joomuk jirugi</li> \
+	<li>Orun guburyo junbi sogi A</li> \
+	<li>Wen kaunde yopcha jirugi</li> \
+	<li>Wen gunnun so bandae ap palkup taerigi (taerigi wen sonbadak)</li> \
+	<li>Wen guburyo junbi sogi A</li> \
+	<li>Orun kaunde yopcha jirugi</li> \
+	<li>Orun gunnun so bandae ap palkup taerigi (taerigi orun sonbadak)</li> \
+	<li>Orun niunja so sang sonkal daebi makgi</li> \
+	<li>Orun gunnun so kaunde sun sonkut tulgi</li> \
+	<li>Wen niunja so sang sonkal daebi makgi</li> \
+	<li>Wen gunnun so kaunde sun sonkut tulgi</li> \
+	<li>Wen gunnun so nopunde bakat palmok yop makgi</li> \
+	<li>Wen gunnun so kaunde bandae ap joomuk jirugi</li> \
+	<li>Orun gunnun so nopunde bakat palmok yop makgi</li> \
+	<li>Orun gunnun so kaunde bandae ap joomuk jirugi</li> \
+	<li>Twigi e wen kyocha so nopunde dung joomuk yop taerigi</li> \
+	<li>Orun gunnun so nopunde doo palmok makgi</li> \
+	<li>(Orunbal e wenbal) wen gunnung so nopunde doo palmok makgi</li> \
+</ol> \
+Narani junbi sogi (wenbal e orunbal) \
+";
+var yul_gok_english = "Ready posture: Parallel ready stance<br> \
+<ol> \
+	<li>Sitting stance (measure left fist, normal motion)</li> \
+	<li>Sitting stance right fist middle front punch (fast motion)</li> \
+	<li>Sitting stance left fist middle front punch (fast motion)</li> \
+	<li>(Left foot to right foot) sitting stance (measure right fist, normal motion)</li> \
+	<li>Sitting stance left fist middle front punch (fast motion)</li> \
+	<li>Sitting stance right fist middle front punch (fast motion)</li> \
+	<li>Right walking stance middle inner forearm side block</li> \
+	<li>Left low front snap kick (hands as in 7)</li> \
+	<li>Left walking stance middle obverse punch (fast motion)</li> \
+	<li>Left walking stance middle reverse punch (fast motion)</li> \
+	<li>Left walking stance inner forearm middle side block</li> \
+	<li>Right low front snap kick (hands as in 11)</li> \
+	<li>Right walking stance middle obverse punch (fast motion)</li> \
+	<li>Right walking stance middle reverse punch (fast motion)</li> \
+	<li>Right walking stance palm middle hooking block</li> \
+	<li>Right walking stance palm middle reverse hooking block (connecting motion)</li> \
+	<li>Right walking stance middle punch (connecting motion)</li> \
+	<li>Left walking stance palm middle hooking block</li> \
+	<li>Left walking stance palm middle reverse hooking block (connecting motion)</li> \
+	<li>Left walking stance middle punch (connecting motion)</li> \
+	<li>Right walking stance middle punch</li> \
+	<li>Right bending ready stance A</li> \
+	<li>Left middle side piercing kick</li> \
+	<li>Left walking stance front elbow reverse strike, striking the left palm</li> \
+	<li>Left bending ready stance A</li> \
+	<li>Right middle side piercing kick</li> \
+	<li>Right walking stance front elbow reverse strike, striking the right palm</li> \
+	<li>Right L-stance twin knife-hand block</li> \
+	<li>Right walking stance straight fingertip middle thrust</li> \
+	<li>Left L-stance twin knife-hand block</li> \
+	<li>Left walking stance straight fingertip middle thrust</li> \
+	<li>Left walking stance outer forearm high side block</li> \
+	<li>Left walking stance middle reverse punch</li> \
+	<li>Right walking stance outer forearm high side block</li> \
+	<li>Right walking stance middle reverse punch</li> \
+	<li>Jump to left X-stance back fist high side strike</li> \
+	<li>Right walking stance double forearm high block</li> \
+	<li>Left walking stance double forearm high block</li> \
+</ol> \
+Parallel ready stance (left foot to right foot) \
+";
+
+var joong_gun_korean = "Junbi jase: Moa junbi sogi B<br> \
+<ol> \
+	<li>Orun niunja so kaunde bandae sonkal makgi</li> \
+	<li>Wen najunde yop apcha busigi (son 1 lo)</li> \
+	<li>Wen dwitbal so bandae sonbadak ollyo makgi</li> \
+	<li>Wen niunja so kaunde bandae sonkal makgi</li> \
+	<li>Orun najunde yop apcha busigi (son 4 lo)</li> \
+	<li>Orun dwitbal so bandae sonbadak ollyo makgi</li> \
+	<li>Orun niunja so kaunde sonkal daebi makgi</li> \
+	<li>Wen gunnun so bandae wi palkup taerigi</li> \
+	<li>Wen niunja so kaunde sonkal daebi makgi</li> \
+	<li>Orun gunnun so bandae wi palkup taerigi</li> \
+	<li>Wen gunnun so nopunde sang joomuk sewo jirugi</li> \
+	<li>Orun gunnun so sang joomuk dwijibo jirugi</li> \
+	<li>(Dwiro dora, gujari dolgi) wen gunnun so nopunde kyocha joomuk chookyo makgi</li> \
+	<li>Orun niunja so nopunde dung joomuk yop taerigi</li> \
+	<li>Wen gunnun so, jappyosul tae (barun dongjak)</li> \
+	<li>Wen gunnun so nopunde bandae jirugi (barun dongjak)</li> \
+	<li>Wen niunja so nopunde dung joomuk yop taerigi</li> \
+	<li>Orun gunnun so, jappyosul tae (barun dongjak)</li> \
+	<li>Orun gunnun so nopunde bandae jirugi (barun dongjak)</li> \
+	<li>Wen gunnun so nopunde doo palmok makgi</li> \
+	<li>Orun niunja so kaunde yop jirugi</li> \
+	<li>Orun kaunde yopcha jirugi</li> \
+	<li>Orun gunnun so nopunde doo palmok makgi</li> \
+	<li>Wen niunja so kaunde yop jirugi</li> \
+	<li>Wen kaunde yopcha jirugi</li> \
+	<li>Orun niunja so kaunde palmok daebi makgi</li> \
+	<li>Wen nachuo so bandae sonbadak noollo makgi (neurin dongjak)</li> \
+	<li>Wen niunja so kaunde palmok daebi makgi</li> \
+	<li>Orun nachuo so bandae sonbadak noollo makgi (neurin dongjak)</li> \
+	<li>(Wenbal e orunbal) moa so orun giokja jirugi (neurin dongjak)</li> \
+	<li>Orun gojung so digutja makgi</li> \
+	<li>Wen gojung so digutja makgi</li> \
+</ol> \
+Moa junbi sogi B (wenbal e orunbal) \
+";
+var joong_gun_english = "Ready posture: Closed ready stance B<br> \
+<ol> \
+	<li>Right L-stance middle reverse knife-hand block</li> \
+	<li>Left low side front snap kick (hands as in 1)</li> \
+	<li>Left rear foot stance reverse palm upward block</li> \
+	<li>Left L-stance middle reverse knife-hand block</li> \
+	<li>Right low side front snap kick (hands as in 4)</li> \
+	<li>Right rear foot stance reverse palm upward block</li> \
+	<li>Right L-stance middle knife-hand guarding block</li> \
+	<li>Left walking stance reverse upper elbow strike</li> \
+	<li>Left L-stance middle knife-hand guarding block</li> \
+	<li>Right walking stance reverse upper elbow strike</li> \
+	<li>Left walking stance high twin fist vertical punch</li> \
+	<li>Right walking stance twin fist upset punch</li> \
+	<li>(Spot turn) left walking stance high X-fist rising block</li> \
+	<li>Right L-stance high back-fist side strike</li> \
+	<li>Left walking stance, release from grab (fast motion)</li> \
+	<li>Left walking stance high reverse punch (fast motion)</li> \
+	<li>Left L-stance high back-fist side strike</li> \
+	<li>Right walking stance, release from grab (fast motion)</li> \
+	<li>Right walking stance high reverse punch (fast motion)</li> \
+	<li>Left walking stance high double forearm block</li> \
+	<li>Right L-stance middle side punch</li> \
+	<li>Right middle side piercing kick</li> \
+	<li>Right walking stance high double forearm block</li> \
+	<li>Left L-stance middle side punch</li> \
+	<li>Left middle side piercing kick</li> \
+	<li>Right L-stance middle forearm guarding block</li> \
+	<li>Left low stance reverse palm pressing block (slow motion)</li> \
+	<li>Left L-stance middle forearm guarding block</li> \
+	<li>Right low stance reverse palm pressing block (slow motion)</li> \
+	<li>(Left foot to right foot) closed stance right angle punch (slow motion)</li> \
+	<li>Right fixed stance U-shape block</li> \
+	<li>Left fixed stance U-shape block</li> \
+</ol> \
+Closed ready stance B (left foot to right foot) \
 ";
 // ######################################
 // ########## PASSWORD MODAL ############
@@ -367,71 +529,18 @@ function switch_lang() {
 	}
 }
 
-function saju_jirugi() {
-	var pattern = document.getElementById("saju-jirugi-korean");
-	if (pattern.classList.contains('english')) {
-		pattern.innerHTML = saju_jirugi_korean;
-		pattern.classList.replace('english', 'korean');
-	} else if (pattern.classList.contains('korean')) {
-		pattern.innerHTML = saju_jirugi_english;
-		pattern.classList.replace('korean', 'english');
+function pattern_translate(pattern) {
+	console.log(pattern+"-korean");
+	var pattern_name = document.getElementById(pattern+"-korean");
+	if (pattern_name.classList.contains('english')) {
+		pattern_name.innerHTML = window[pattern+'_korean'];
+		pattern_name.classList.replace('english', 'korean');
+	} else if (pattern_name.classList.contains('korean')) {
+		pattern_name.innerHTML = window[pattern+'_english'];
+		pattern_name.classList.replace('korean', 'english');
 	}
 }
 
-function saju_makgi() {
-	var pattern = document.getElementById("saju-makgi-korean");
-	if (pattern.classList.contains('english')) {
-		pattern.innerHTML = saju_makgi_korean;
-		pattern.classList.replace('english', 'korean');
-	} else if (pattern.classList.contains('korean')) {
-		pattern.innerHTML = saju_makgi_english;
-		pattern.classList.replace('korean', 'english');
-	}
-}
-
-function chonji() {
-	var pattern = document.getElementById("chonji-korean");
-	if (pattern.classList.contains('english')) {
-		pattern.innerHTML = chonji_korean;
-		pattern.classList.replace('english', 'korean');
-	} else if (pattern.classList.contains('korean')) {
-		pattern.innerHTML = chonji_english;
-		pattern.classList.replace('korean', 'english');
-	}
-}
-
-function dan_gun() {
-	var pattern = document.getElementById("dan-gun-korean");
-	if (pattern.classList.contains('english')) {
-		pattern.innerHTML = dan_gun_korean;
-		pattern.classList.replace('english', 'korean');
-	} else if (pattern.classList.contains('korean')) {
-		pattern.innerHTML = dan_gun_english;
-		pattern.classList.replace('korean', 'english');
-	}
-}
-
-function do_san() {
-	var pattern = document.getElementById("do-san-korean");
-	if (pattern.classList.contains('english')) {
-		pattern.innerHTML = do_san_korean;
-		pattern.classList.replace('english', 'korean');
-	} else if (pattern.classList.contains('korean')) {
-		pattern.innerHTML = do_san_english;
-		pattern.classList.replace('korean', 'english');
-	}
-}
-
-function won_hyo() {
-	var pattern = document.getElementById("won-hyo-korean");
-	if (pattern.classList.contains('english')) {
-		pattern.innerHTML = won_hyo_korean;
-		pattern.classList.replace('english', 'korean');
-	} else if (pattern.classList.contains('korean')) {
-		pattern.innerHTML = won_hyo_english;
-		pattern.classList.replace('korean', 'english');
-	}
-}
 // ###############################
 // ######### NEWS PAGE ###########
 // ###############################
